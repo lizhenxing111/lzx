3eddfd8ecd5955786e210c15b7ca0ba4ae3aaea0
Administrator <15110124387@163.com>
2019-01-31 16:56:35 +0800

    1

---
+++ lzx/src/main/java/org/lzx/server/LzxApplication.java
@@ -0,0 +1,16 @@
+package org.lzx.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class LzxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LzxApplication.class, args);
+	}
+
+}
+
