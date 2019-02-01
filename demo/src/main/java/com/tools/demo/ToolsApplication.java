ba7e43223af980ef88d92b06884df9b6ab660527
Administrator <15110124387@163.com>
2019-02-01 13:44:31 +0800

    A

--- demo/src/main/java/com/tools/demo/ToolsApplication.java
+++ demo/src/main/java/com/tools/demo/ToolsApplication.java
@@ -3,12 +3,14 @@ package com.tools.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.tools.demo.*.dao")
 @EnableCaching
+@EnableEurekaClient
 public class ToolsApplication {
 
 	public static void main(String[] args) {
--- demo/src/main/java/com/tools/demo/generator/controller/Generator.java
+++ demo/src/main/java/com/tools/demo/generator/controller/Generator.java
@@ -1,10 +1,12 @@
 package com.tools.demo.generator.controller;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@EnableEurekaClient
 @RequestMapping("/")
 public class Generator {
 
